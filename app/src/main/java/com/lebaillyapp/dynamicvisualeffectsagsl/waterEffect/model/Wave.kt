0031071c9c51822ce041409f89898fb33e7f828d
Samuel Lebailly <sam.meusa@gmail.com>
2025-07-03 21:22:16 +0200

    feature(waterEffect/model): Add data model object for the waves.

---
+++ app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/waterEffect/model/Wave.kt
@@ -0,0 +1,11 @@
+package com.lebaillyapp.dynamicvisualeffectsagsl.waterEffect.model
+
+import androidx.compose.ui.geometry.Offset
+
+data class Wave(
+    val origin: Offset,
+    val startTime: Float, // Changé de Long à Float pour être cohérent avec les secondes
+    val amplitude: Float,
+    val frequency: Float,
+    val speed: Float
+)
