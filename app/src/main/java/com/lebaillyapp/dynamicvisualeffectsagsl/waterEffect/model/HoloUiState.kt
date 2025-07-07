7a9b563fef41069be8f2e7e225d94d8697241a35
Samuel Lebailly <sam.meusa@gmail.com>
2025-07-07 21:59:54 +0200

    feature(model): Create a data model object to manage HoloUI states.

---
+++ app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/waterEffect/model/HoloUiState.kt
@@ -0,0 +1,8 @@
+package com.lebaillyapp.dynamicvisualeffectsagsl.waterEffect.model
+
+data class HoloUiState(
+    val tiltX: Float = 0f,
+    val tiltY: Float = 0f,
+    val time: Float = 0f,
+    val pulse: Float = 0f // pour les effets tactiles plus tard
+)
