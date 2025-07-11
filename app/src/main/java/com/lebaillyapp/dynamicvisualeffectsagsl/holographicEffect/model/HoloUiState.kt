cd9b441cb603507b3b593fd7d9354a246ba213e8
Samuel Lebailly <sam.meusa@gmail.com>
2025-07-11 20:57:25 +0200

    feature(holographicEffect/model): Add new parameter to the HoloUiState to manage touch input.

--- app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/holographicEffect/model/HoloUiState.kt
+++ app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/holographicEffect/model/HoloUiState.kt
@@ -4,5 +4,7 @@ data class HoloUiState(
     val tiltX: Float = 0f,
     val tiltY: Float = 0f,
     val time: Float = 0f,
-    val pulse: Float = 0f // pour les effets tactiles plus tard
+    val pulse: Float = 0f,
+    val touchX: Float = 0f,
+    val touchY: Float = 0f
 )
