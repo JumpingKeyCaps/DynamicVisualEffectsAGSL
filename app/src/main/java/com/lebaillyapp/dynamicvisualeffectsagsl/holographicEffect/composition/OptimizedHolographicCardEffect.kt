20478e774ee07bf37b6ee6a2db9011479cd2c2cd
Samuel Lebailly <sam.meusa@gmail.com>
2025-07-17 15:53:25 +0200

    feature(holographicEffect/composition): minor tweak to generate screenshots.

--- app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/holographicEffect/composition/OptimizedHolographicCardEffect.kt
+++ app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/holographicEffect/composition/OptimizedHolographicCardEffect.kt
@@ -44,7 +44,7 @@ fun OptimizedHolographicCardEffect(
     chromaticAberrationStrength: Float = 1.5f,      // Force du décalage RVB sur le roll
 
     effectIntensity: Float = 2.0f,                  // Multiplicateur final de l’effet arc-en-ciel
-    fresnelPower: Float = 6.0f,                     // Puissance du falloff directionnel (style "reflet")
+    fresnelPower: Float = 1.0f,                     // Puissance du falloff directionnel (style "reflet")
     rainbowScale: Float = 10.2f,                     // Taille des "taches d’huile" colorées
     rainbowOffset: Float = 1.0f,                    // Décalage global du motif rainbow
     normalStrength: Float = 8.5f,                   // Force du relief (impacte la déformation du pattern)
