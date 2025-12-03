c0b34cc4c1911659d5e67befbe4209d816440618
Samuel Lebailly <sam.meusa@gmail.com>
2025-12-03 03:58:55 +0100

    feature(main/): Clean the main activity to now use an navigation host to display a main menu with all shaders demo available.

--- app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/dynamicvisualeffectsagsl/MainActivity.kt
@@ -4,117 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
-import com.lebaillyapp.dynamicvisualeffectsagsl.holographicEffect.composition.HolographicEffectBitmapShader
-import com.lebaillyapp.dynamicvisualeffectsagsl.holographicEffect.composition.OptimizedHolographicCardEffect
-import com.lebaillyapp.dynamicvisualeffectsagsl.holographicEffect.composition.UltraRealisticHolographicEffectShader
-import com.lebaillyapp.dynamicvisualeffectsagsl.topographicflowEffect.TopographicFlowShader
-import com.lebaillyapp.dynamicvisualeffectsagsl.topographicflowEffect.TopographicFlowShaderWithControls
+import com.lebaillyapp.dynamicvisualeffectsagsl.navigation.AppNavHost
 import com.lebaillyapp.dynamicvisualeffectsagsl.ui.theme.DynamicVisualEffectsAGSLTheme
-import kotlin.math.cos
-import kotlin.math.sin
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
-            //default bitmap
-            val bitmap = ImageBitmap.imageResource(id = R.drawable.de2)
-
             DynamicVisualEffectsAGSLTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-
-                    // [1] - Water Effect composition screen
-
-                    /**
-
-                    WaterEffectBitmapShader(
-                    modifier = Modifier.fillMaxSize(),
-                    bitmap = bitmap,
-                    shaderResId = R.raw.water_shader
-                    )
-
-                     */
-
-
-
-
-                    // [2] - Holographic Effect composition screen
-
-                    /**
-                    HolographicEffectBitmapShader(
-                    bitmap = bitmap,
-                    shaderResId = R.raw.holographic_base_shader,
-                    intensity = 0.99f,        // Intensité globale
-                    shimmerSpeed = 1.2f,     // Vitesse d'animation
-                    tiltAngle = 0.5f         // Angle de base pour les reflets
-                    )
-                     */
-
-                    /**v3
-                    UltraRealisticHolographicEffectShader(
-                    modifier = Modifier.fillMaxSize(),
-                    bitmap = bitmap,
-                    shaderResId = R.raw.holographic_realistic_shader,
-                    // Paramètres pertinents pour l'effet arc-en-ciel modulé par Fresnel
-                    effectIntensity = 6.8f,
-                    fresnelPower = 6.0f,
-                    rainbowScale = 1.2f,
-                    rainbowOffset = 0.2f,
-                    normalStrength = 2.0f,
-                    microDetailScale = 45.0f
-                    )
-
-
-                     */
-
-                    /**v3
-                    OptimizedHolographicCardEffect(
-                    modifier = Modifier.fillMaxSize(),
-                    bitmap = bitmap,
-                    shaderResId = R.raw.holographic_card_shader,
-
-                    )
-                     **/
-
-
-                    // [3] - Liquid Topographic Flow composition screen
-                    TopographicFlowShader(modifier = Modifier)
-
-                 //   TopographicFlowShaderWithControls(modifier = Modifier)
-
-
-
-                    // [X] - Fire Effect composition screen
-
-                    /**
-                    FireEffectScreen(
-                    modifier = Modifier.fillMaxSize(),
-                    bitmap = bitmap,
-                    shaderResId = R.raw.fire_shader
-                    )
-
-                     */
-
-
-                }
+                AppNavHost()
             }
         }
     }
